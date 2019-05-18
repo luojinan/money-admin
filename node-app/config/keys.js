9725d4fdc6b53aacd7a3969c41014c2d9f026fde
罗锦安 <1017386624@qq.com>
2019-05-18 12:29:08 +0800

    在简易服务器中添加简易本地数据库连接

---
+++ node-app/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // mongoUrl : "mongodb+srv://test:test123456@cluster0-dkkcy.mongodb.net/test?retryWrites=true"  //连接超时，ip地址原因，但是解决不了
+  mongoUrl:'mongodb://127.0.0.1:27017/demo2'  //本地mongo数据库
+}
--- node-app/server.js
+++ node-app/server.js
@@ -1,8 +1,15 @@
 // 创建服务器，并简单配置开启
 const express = require('express')
+const mongoose = require('mongoose')  // 引入数据库mongoose模块
+const keys = require('./config/keys') //引入配置常量
 
 //实例化服务器(不用new？)
 const app = express()
+// 连接数据库
+console.log(keys.mongoUrl)
+mongoose.connect(keys.mongoUrl,{ useNewUrlParser: true })
+        .then(()=>{console.log('mongodb connected')})
+        .catch(err=>{console.log(err)})
 
 //端口号，配置成变量，方便分成生产环境(上线)和开发环境(本地)
 // process.env.PORT是打包配置的变量（这里暂时没配置）
