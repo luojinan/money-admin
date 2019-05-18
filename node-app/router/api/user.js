9455f4395d7a0981d94c5031df00ae462ec86a6b
罗锦安 <1017386624@qq.com>
2019-05-18 19:33:10 +0800

    完善注册接口，使用gravatar头像模块

--- node-app/router/api/user.js
+++ node-app/router/api/user.js
@@ -3,12 +3,13 @@ const express = require('express')  //引入express
 const router = express.Router()     //使用express的路由实例
 
 const bcrypt = require('bcrypt')    //引入加密模块（处理密码）在npm官网查看使用详情
+const gravatar = require('gravatar')  // 引入头像依赖
 
 // 引入已连接的数据库表格
 const UserModel = require('./../../models/User.js')
 
 // 配置路由详细情况，路径末端名（qpi名），响应方法
-// 变成规范，注释写好接口的路径和简介
+// 编程规范，注释写好接口的路径和简介
 /**
  * $route   GET api/users/test
  * @desc    测试返回一个写死的json对象
@@ -26,12 +27,16 @@ router.get("/test",(req,res)=>{
 router.post("/register",(req,res)=>{
   // res.json(req.body)  //postman测试接收到的post数据
   UserModel.findOne({email:req.body.email})
-          .then((user)=>{
+           .then((user)=>{
             // 判断是邮箱已存在
             if(user) return res.status(400).json({email:"邮箱已被占用！"})  // 编写接口请求失败提示语法
             else {
+              // 调用头像依赖
+              const avatar = gravatar.url(req.body.email, {s: '200', r: 'pg', d: 'mm'})  // 'mm'是默认头像
+
               const newUser = new UserModel({
                 name:req.body.name,
+                avatar,
                 email:req.body.email,
                 password:req.body.password
               })
@@ -47,7 +52,7 @@ router.post("/register",(req,res)=>{
                 })
               })
             }
-          })
+           })
 })
 
 // 暴露路由
