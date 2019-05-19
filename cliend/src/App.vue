209da997ddd2758a1ec5eadc53d124d6f0b4d79d
罗锦安 <1017386624@qq.com>
2019-05-19 20:49:45 +0800

    通过在根页面app中添加解析token存入vuex的方法，这样刷新也会再执行一次存入

--- cliend/src/App.vue
+++ cliend/src/App.vue
@@ -4,6 +4,25 @@
   </div>
 </template>
 
+<script>
+import jwtDecode from 'jwt-decode'      //引入解析token模块
+import {isEmpty} from './utils/util'   //引入工具方法，判断值是否为空
+
+export default {
+  name:'app',
+  created(){
+    // 解决刷新就把vuex里的数据清空的问题，这里写一个，在刷新时，会执行
+    if(localStorage.eleToken){
+      const result = jwtDecode(localStorage.eleToken)
+      this.$store.dispatch('setAuthenticated',!isEmpty(result))
+      // 解析后的值存入vuex用户
+      this.$store.dispatch('setUser',result)
+    }
+  }
+}
+</script>
+
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
