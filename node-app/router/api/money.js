e328570ed55ad7f318fb993c48ebf90a68c7902c
罗锦安 <1017386624@qq.com>
2019-05-20 23:36:30 +0800

    增加编辑单条资金数据，修改赋值方式，解决赋值空字符串的问题。增加根据id删除资金数据

--- node-app/router/api/money.js
+++ node-app/router/api/money.js
@@ -39,16 +39,46 @@ router.post('/addMoney',passport.authenticate('jwt',{session:false}),(req,res)=>
   // 添加数据
   const body = req.body
   const moneyList = {}
-  moneyList.type = body.type ? body.type : ''
-  moneyList.describe = body.describe ? body.describe : ''
-  moneyList.incode = body.incode ? body.incode : ''
-  moneyList.expend = body.expend ? body.expend : ''
-  moneyList.cash = body.cash ? body.cash : ''
-  moneyList.remark = body.remark ? body.remark : ''
+  if(body.type) moneyList.type = body.type
+  if(body.describe) moneyList.describe = body.describe
+  if(body.incode) moneyList.incode = body.incode
+  if(body.expend) moneyList.expend = body.expend
+  if(body.cash) moneyList.cash = body.cash
+  if(body.remark) moneyList.remark = body.remark
   
   new MoneyModel(moneyList).save().then(data=>{
     res.json(data)
   })
+})
+/**
+ * $route   POST api/money/edit/:id
+ * @desc    根据id修改某条资金详情信息，要登录才能调用
+ * @access  public公开接口（是否需要token登录）
+ */
+router.post('/edit/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  // 修改数据
+  const body = req.body
+  const moneyList = {}
+  if(body.type) moneyList.type = body.type
+  if(body.describe) moneyList.describe = body.describe
+  if(body.incode) moneyList.incode = body.incode
+  if(body.expend) moneyList.expend = body.expend
+  if(body.cash) moneyList.cash = body.cash
+  if(body.remark) moneyList.remark = body.remark
+  
+  MoneyModel.findOneAndUpdate({_id:req.params.id},{$set:moneyList},{new:true}).then(data=>{
+    res.json(data)
+  })
+})
+/**
+ * $route   DELETE api/money/delete/:id
+ * @desc    根据id表格的一条数据
+ * @access  public公开接口（是否需要token登录）
+ */
+router.delete('/delete/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  MoneyModel.findOneAndRemove({_id:req.params.id}).then(data=>{
+    data.save().then(data=> res.json(data))
+  }).catch(err=>res.status(404).json('删除失败'))
 })
 
 module.exports = router
