d6a9d355d9f0c7d85b2ceacc7b49007b025ffefc
罗锦安 <1017386624@qq.com>
2019-05-20 22:44:01 +0800

    跟post一样设置token就可以设置数据与用户匹配了，不用对数据库做区分。添加根据资金id获取数据的接口api路由，利用/:id，获取req.params.id，与前端vue路由相似

--- node-app/router/api/money.js
+++ node-app/router/api/money.js
@@ -10,15 +10,29 @@ const MoneyModel = require('../../models/Money')
 
 /**
  * $route   GET api/money
- * @desc    获取资金详情表格，不是对应用户的？后期补充
+ * @desc    获取资金详情表格
  * @access  public公开接口（是否需要token登录）
  */
-router.get('/',(req,res)=>{
-  MoneyModel.find().then( data => res.json(data) )
+router.get('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  MoneyModel.find().then( data => {
+    if(!data) return res.status(404).json('没有任何内容')
+    res.json(data)
+  } ).catch(err=>res.status(404).json(err))
+})
+/**
+ * $route   GET api/money/:id
+ * @desc    根据id表格的一条数据
+ * @access  public公开接口（是否需要token登录）
+ */
+router.get('/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+  MoneyModel.findOne({_id:req.params.id}).then( data => {
+    if(!data) return res.status(404).json('输入的资金id没有数据')
+    res.json(data)
+  } ).catch(err=>res.status(404).json(err))
 })
 /**
  * $route   POST api/money/addMoney
- * @desc    添加资金详情信息，要登录才能调用，但是上传到数据库的这条数据没有用用户区分
+ * @desc    添加资金详情信息，要登录才能调用
  * @access  public公开接口（是否需要token登录）
  */
 router.post('/addMoney',passport.authenticate('jwt',{session:false}),(req,res)=>{
